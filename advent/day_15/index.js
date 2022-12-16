ab7e4cfb4c7e61847c799a782a8ff980d639b432
fones <ja@fones.pl>
2022-12-16 21:03:44 +0100

    Day 15 📶 Part 2

--- advent/day_15/index.js
+++ advent/day_15/index.js
@@ -30,9 +30,6 @@ const maxMD = Math.max(...data.map(([s, b, md]) => md))
 maxX = maxX + maxMD
 minX = minX - maxMD
 
-// console.log('maxX', maxX)
-// console.log('minX', minX)
-
 const partOne = (data, y) => {
   // const points = new Array(maxX - minX).fill('.')
   const pointSet = new Set()
@@ -64,3 +61,43 @@ const partOne = (data, y) => {
 //}
 
 console.log('PART ONE', partOne(data, 2000000))
+
+const partTwo = (data) => {
+  const min = 0
+  const max = 4000000 // 20
+  try {
+    // for each sensor
+    data.forEach(([[sx, sy], b, d], si) => {
+      // find all points where d1 = d + 1
+      const d1 = d + 1
+      let points = []
+      for (let i = -d1; i <= d1; i++) {
+        px  = sx + i
+        py1 = sy + (d1 - Math.abs(i))
+        py2 = sy - (d1 - Math.abs(i))
+        points.push([px, py1], [px, py2])
+      }
+      // exclude points outside the scope (min, max)
+      points = points.filter( ([x, y]) => min <= x && x <= max && min <= y && y <= max )
+      // for each other sensor
+      data.forEach(( [os, ob, od], sj) => {
+        // skip the same sensor
+        if (si == sj) return
+        // leave points that are outside the range
+        points = points.filter(p => md(os, p) > od)
+      })
+      // after all sensor, check if any point left
+      if (points.length > 0) {
+        // this is the point
+        throw points[0]
+      } 
+    })
+    return null
+  } catch (thisPoint) {
+    console.log('Found beacon at:', thisPoint)
+    const [x, y] = thisPoint
+    return x * 4000000 + y
+  }
+}
+
+console.log('PART TWO', partTwo(data))
